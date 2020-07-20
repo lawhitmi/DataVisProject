adaaa4aa0582ce2525157cf6f4db42dbb609631e
Edpape007 <edgardo.panza@gmail.com>
2020-07-20 11:22:44 +0200

    changes in graphs

--- Ed_Vis.R
+++ Ed_Vis.R
@@ -8,6 +8,9 @@ library(packcircles)
 library(latticeExtra)
 library(reshape2)
 library(RColorBrewer)
+library(PCAmixdata)
+library(plotly)
+library(gridExtra)
 
 #Start cleaning the environment 
 rm(list=ls())
@@ -369,5 +372,131 @@ radarchart( PivotData  , axistype=1 ,
 
 # Add a legend
 legend(x=1, y=1, legend = rownames(PivotData[-c(1,2),]), bty = "n", pch=20 , col=colors_in , text.col = "grey", cex=1.0, pt.cex=3)
+
+
+
+##### Changing to space flight hours
+
+
+data.gender <- select(data, Year, Gender,Space.Flight..hr.,Space.Walks..hr.)
+data.genderNotNA <- filter(data.gender, Year !=  'NA')
+
+data.gender <- data.genderNotNA[order(data.genderNotNA$Year),]
+
+TotalRecruit <- as.data.frame(table(data.gender[,1:2]))
+colnames(TotalRecruit) <- c('Year','Gender','Total')
+TotalRecruit$Year <- as.numeric(as.character(TotalRecruit$Year))
+str(TotalRecruit)
+TotalRecruit <- TotalRecruit[order(TotalRecruit[,1],TotalRecruit[,2]),]
+
+Recruit <- as.data.frame((table(data.gender$Year)))
+colnames(Recruit) <- c('Year','Total')
+
+#totalRecruitXGender <- data.gender %>% count(Year, Gender)
+
+for (row in 1:nrow(Recruit)) {
+  year <- Recruit[row,1]
+  total <- Recruit[row,2]
+  
+  for (i in 1:nrow(TotalRecruit)){
+    if(year == TotalRecruit[i,1]){
+      TotalRecruit[i,4] <- total
+      TotalRecruit[i,5] <- TotalRecruit[i,3] / total
+    }
+  }
+}
+colnames(TotalRecruit) <- c('Year','Gender','Total_Gender_Enroll','Total_Enroll_Year','Proportion_Enroll')
+
+
+
+TotalFlightHours <- data.genderNotNA %>% 
+  group_by(Year) %>% 
+  summarise_at(vars(Space.Flight..hr.),
+               list(TotalFlight=sum))
+
+data.hourGender <- data.genderNotNA %>% 
+  group_by(Year,Gender) %>% 
+  summarise_at(vars(Space.Flight..hr.),
+               list(TotalFlight=sum))
+
+
+for (row in 1:nrow(TotalFlightHours)) {
+  year <- TotalFlightHours[row,1]
+  total <- TotalFlightHours[row,2]
+  
+  for (i in 1:nrow(data.hourGender)){
+    if(year == data.hourGender[i,1]){
+      data.hourGender[i,4] <- (data.hourGender[i,3] / total)
+    }
+  }
+}
+colnames(data.hourGender) <- c('Year','Gender','TotalWalk',"Proportion_Flight")
+
+
+TotalProportions <- merge(TotalRecruit,data.hourGender,by=c("Year",'Gender'))
+
+
+FemaleProportions <- TotalProportions[TotalProportions$Gender == "Female", ]
+
+FemaleProportions <- FemaleProportions[,-c(3,4,6)]
+
+piv1 = dcast(FemaleProportions, Gender ~ Year, value.var = c("Proportion_Enroll"))
+piv2 = dcast(FemaleProportions, Gender ~ Year, value.var = c("Proportion_Flight"))
+
+PivotData <- rbind(piv1,piv2)
+
+PivotData[is.na(PivotData)] <- 0
+PivotData = PivotData[2:ncol(PivotData)]
+rownames(PivotData) <- c("Enroll", "Flight")
+
+Min = 0
+Max = 1
+
+PivotData <- rbind(rep(Max,20) , rep(Min,20) , PivotData)
+
+colors_border=c( '#1B9E77E6','#D95F02E6' )
+colors_in=c( '#1B9E7766','#D95F0266')
+
+radarchart( PivotData  , axistype=1,
+            #custom polygon
+            pcol=colors_border , pfcol=colors_in , plwd=1 , plty=1,
+            #custom the grid
+            cglcol="grey", cglty=2, axislabcol="grey", caxislabels=seq(0,1,1), cglwd=0.6,
+            #custom labels
+            vlcex=0.8 ,title=paste("Female Enrollment Vs Space Flights"),
+)
+
+# Add a legend
+legend(x=1.2, y=1.2, legend = rownames(PivotData[-c(1,2),]), bty = "n", pch=20 , col=colors_in , text.col = "grey", cex=2.0, pt.cex=3)
+
+#####Conditional Density plot
+
+data <- read.csv("datasets_934_1711_astronauts.csv",na.strings=c(""," ","NA")) #replace blanks with 'NA'
+data.gender <- select(data, Year, Gender,Space.Flight..hr.,Space.Walks..hr.)
+data.genderNotNA <- filter(data.gender, Year !=  'NA')
+
+
+gby1 <- data.genderNotNA %>% 
+  group_by(Year,Gender) %>% 
+  summarise_at(vars(Space.Flight..hr.),
+               list(TotalFlight=sum))
+
+gby2 <- data.genderNotNA %>% 
+  group_by(Year,Gender) %>% 
+  summarise_at(vars(Space.Walks..hr.),
+               list(TotalWalk=sum))
+
+
+data.hourGender <- merge(gby1,gby2,by=c("Year","Gender"))
+
+data.hourGender[3:4] <- lapply(data.hourGender[3:4], function(x) c(scale(x)))
+
+c <- c("Year", "Gender")
+
+ggplotly(ggplot(data.hourGender, aes_string(data.hourGender$TotalFlight, fill = data.hourGender$Gender)) + 
+           geom_density(position='fill', alpha = 0.5) + 
+           xlab("Flight Hours") + labs(fill='Gender') + 
+           theme(legend.text=element_text(size=12), 
+                 axis.title=element_text(size=14)))
 
 
